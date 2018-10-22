825d76cc86e9f6cf3896f37f5ca4426f11d29404
Pulkit gera <pulkithifi@yahoo.com>
2018-10-22 16:49:35 +0530

    basic functions working

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Chatbot
-This chatbot was made for opengift hackathon challenge
+This chatbot was made for opengift hackathon challenge  
+The chatbot will have 2 components. The first component will have the code which will act as a chat bot. The second will be an admin interface which will return useful statistics.
+To know more about the details of the hackathon [click here](https://opengift.io/hackathon/#current) 
---
+++ bot.py
@@ -0,0 +1,33 @@
+import json
+import requests
+
+TOKEN = "735102963:AAGHS0kuDlBCfqypMY0W7o4kqJChRvDQX0U"
+URL = "https://api.telegram.org/bot{}/".format(TOKEN)
+
+def get_url(url):
+    response = requests.get(url)
+    content = response.content.decode("utf8")
+    return content
+
+def get_json_from_url(url):
+    content = get_url(url)
+    js = json.loads(content)
+    return js
+
+def get_updates():
+    url = URL+"getUpdates"
+    content = get_json_from_url(url)
+    return content
+
+def getLastChat(content):
+    text = content['result'][-1]['message']['text']
+    id = content['result'][-1]['message']['chat']['id']
+    return (text,id)
+
+def sendMessage(text,chat_id):
+    url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
+    get_url(url)
+
+content = get_updates()
+text , chat_id = getLastChat(content)
+sendMessage("python rocks",chat_id)
